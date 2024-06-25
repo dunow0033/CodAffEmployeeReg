2816a961c2d041b102583f1b4aed3c16b09263fd
Brian Donnoe <dunow33@hotmail.com>
2024-06-25 10:06:10 -0400

    I think most of the code is in, still stuck on 2 parts of it

---
+++ CodAffEmployeeRegisterAPI/CodAffEmployeeRegisterAPI/Models/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodAffEmployeeRegisterAPI.Models
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+completing this tutorial:
+
+https://www.youtube.com/watch?v=ORVShW0Yjaw&list=PLjC4UKOOcfDSA06HsjJK4ZBr1tnPaOOpq&index=5
